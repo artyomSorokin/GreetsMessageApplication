eab71a389c7a0f257dc1abcae130980eb15b47d5
artyomSorokin <artyom_sorokin@mail.ru>
2016-12-12 23:25:50 +0200

    Update TimeParser.java

--- src/main/java/sorokin/TimeParser.java
+++ src/main/java/sorokin/TimeParser.java
@@ -1,37 +1,30 @@
 package sorokin;
 
 import org.apache.log4j.Logger;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public class TimeParser implements Runnable {
+public class TimeParser {
 
-    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-    private ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
-    protected Map<Enum,String> timeMap;
-    protected Date morning;
-    protected Date day;
-    protected Date evening;
-    protected Date night;
-    protected Date currentTime;
+    protected ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
+    protected String message;
+    protected LocalTime morning;
+    protected LocalTime day;
+    protected LocalTime evening;
+    protected LocalTime night;
+    protected LocalTime currentTime;
     protected static Logger log = Logger.getLogger(ReadFile.class);
-
-    public void setTimeMap(Map<Enum, String> timeMap) {
-        this.timeMap = timeMap;
-    }
 
     protected void parseDate(Map<Enum,String> timeMap) {
         try {
             log.info("Start convert dates from String to Date");
-            morning = simpleDateFormat.parse(timeMap.get(Time.MORNING));
-            day = simpleDateFormat.parse(timeMap.get(Time.DAY));
-            evening = simpleDateFormat.parse(timeMap.get(Time.EVENING));
-            night = simpleDateFormat.parse(timeMap.get(Time.NIGHT));
-            currentTime = simpleDateFormat.parse(simpleDateFormat.format(new Date()));
+            morning = LocalTime.parse(timeMap.get(Time.MORNING));
+            day = LocalTime.parse(timeMap.get(Time.DAY));
+            evening = LocalTime.parse(timeMap.get(Time.EVENING));
+            night = LocalTime.parse(timeMap.get(Time.NIGHT));
+            currentTime = LocalTime.now();
         }catch (Exception e){
             log.info("Problem with convert date");
         }
@@ -39,22 +32,16 @@ public class TimeParser implements Runnable {
 
     protected void defineTime(){
         log.info("Define time, locale and choose message");
-        if (currentTime.after(morning) && currentTime.before(day)) {
-            ShowTime.show(bundle.getString("morning"));
-        } else if (currentTime.after(day) && currentTime.before(evening)) {
-            ShowTime.show(bundle.getString("day"));
-        } else if (currentTime.after(evening) && currentTime.before(night)) {
-            ShowTime.show(bundle.getString("evening"));
+        if ((currentTime.isAfter(morning) && currentTime.isBefore(day)) || currentTime.equals(LocalTime.parse("06:00:00"))) {
+            message = bundle.getString("morning");
+        } else if ((currentTime.isAfter(day) && currentTime.isBefore(evening)) || currentTime.equals(LocalTime.parse("09:00:00"))) {
+            message = bundle.getString("day");
+        } else if ((currentTime.isAfter(evening) && currentTime.isBefore(night)) || currentTime.equals(LocalTime.parse("19:00:00"))) {
+            message = bundle.getString("evening");
         } else {
-            ShowTime.show(bundle.getString("night"));
+            message = bundle.getString("night");
         }
+        ShowTime.show(message);
     }
 
-
-    public void run() {
-        log.info("Start date parser");
-        parseDate(timeMap);
-        log.info("Start define time");
-        defineTime();
-    }
 }
